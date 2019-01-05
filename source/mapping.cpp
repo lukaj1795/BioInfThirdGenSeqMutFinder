c5a7310c64929a307b15368daab4fd69d66cc13a
Ivan Moštak <mostak.ivan@gmail.com>
2019-01-05 23:30:47 +0100

    accuracy boost
    jaccard 0.479

--- source/mapping.cpp
+++ source/mapping.cpp
@@ -11,24 +11,24 @@
 namespace mapping {
 	int error = -99999;
 	
-	/*std::unordered_map<char, int> k1_count = {
+	std::map<char, int> k1_count = {
 		{ 'C', 0 },
 		{ 'A', 0 },
 		{ 'T', 0 },
 		{ 'G', 0 }
 	};
-	std::unordered_map<char, int> k2_count = {
+	std::map<char, int> k2_count = {
 		{ 'C', 0 },
 		{ 'A', 0 },
 		{ 'T', 0 },
 		{ 'G', 0 }
-	};*/
-	std::vector<char> bases = { 'A', 'C', 'T', 'G' };
+	};
+	//std::vector<char> bases = { 'A', 'C', 'T', 'G' };
 	int map_to_reference(std::vector<Kmer>* reference, std::vector<Kmer>* sequence)
 	{
 
 		int kmer_size = reference->front().string.size();
-		int n = 20;
+		int n = 12;
 		int match = 0;
 		int flag1 = 0;
 		int counter = 0;
@@ -38,11 +38,13 @@ namespace mapping {
 		std::map <int,Kmer_mapping> matches;
 		//std::vector<Kmer>::iterator speed_up;
 		counter = 0;
-		int step = 5;
+		int step = 7;
 
+		Kmer kmer_ref = reference->front();
+		Kmer kmer_seq = sequence->front();
 		for (auto kmer_ref_iterator = reference->begin(); kmer_ref_iterator != reference->end(); ++kmer_ref_iterator) {
 			
-			auto kmer_ref = *kmer_ref_iterator;
+			kmer_ref = *kmer_ref_iterator;
 			counter++;
 			if (match == 0) {
 				if (counter < step) {
@@ -50,17 +52,17 @@ namespace mapping {
 				}
 				counter = 0; //reset counter for skipping STEP number of references
 			}
-			if (match > 12) {
+			if (match > 8) {
 				break;
 			}
 			flag1 = 0;
 			for (auto kmer_seq_iterator = sequence->begin(); kmer_seq_iterator != sequence->end(); ++kmer_seq_iterator) {
-				auto kmer_seq = std::move(*kmer_seq_iterator);
+				kmer_seq = *kmer_seq_iterator;
 				flag1++;
-				if (flag1 < 5) {
+				if (flag1 < 3) {
 					continue;
 				}
-				if (flag1 > 99) {
+				if (flag1 > 109) {
 					break;
 				}
 				//if (!mapping::check_match(kmer_ref, kmer_seq)) {
@@ -87,51 +89,28 @@ namespace mapping {
 				}
 			}
 		}
-
+		if (match <= 5) {
+			return -99999;
+		}
 		for (auto it1 = seq_pos_match.begin(); it1 != seq_pos_match.end(); ++it1) {
 			if (std::next(it1, 1) != seq_pos_match.end()) {
 				auto it2 = std::next(it1, 1);
 				if (std::next(it1, 2) != seq_pos_match.end()) {
 					auto it3 = std::next(it1, 2);
 					if (std::next(it1, 3) != seq_pos_match.end()) {
-						auto it4 = std::next(it1, 3);
-						if (std::next(it1, 4) != seq_pos_match.end()) {
-							auto it5 = std::next(it1, 4);
-							if (std::next(it1, 5) != seq_pos_match.end()) {
-								auto it6 = std::next(it1, 5);
+						auto a = matches.at(*it1).reference_positions;
+						auto b = matches.at(*it2).reference_positions;
+						auto c = matches.at(*it3).reference_positions;
+						for (auto aa : a) {
+							for (auto bb : b) {
+								if (bb - aa >= *it2 - *it1 - n && bb - aa <= *it2 - *it1 + n) {
+									for (auto cc : c) {
+										if (cc - bb >= *it3 - *it2 - n && cc - bb <= *it3 - *it2 + n) {
+											return(aa - *it1);
 
-								auto a = matches.at(*it1).reference_positions;
-								auto b = matches.at(*it2).reference_positions;
-								auto c = matches.at(*it3).reference_positions;
-								auto d = matches.at(*it4).reference_positions;
-								auto e = matches.at(*it5).reference_positions;
-								//auto f = matches.at(*it6).reference_positions;
-
-								for (auto aa : a) {
-									
-									for (auto bb : b) {
-										if (bb - aa >= *it2 - *it1 - n && bb - aa <= *it2 - *it1 + n) {
-											for (auto cc : c) {
-												if (cc - bb >= *it3 - *it2 - n && cc - bb <= *it3 - *it2 + n) {
-													for (auto dd : d) {
-														if (dd - cc >= *it4 - *it3 - n && dd - cc <= *it4 - *it3 + n) {
-															for (auto ee : e) {
-																if (ee - dd >= *it5 - *it4 - n && ee - dd <= *it5 - *it4 + n) {
-																	//for (auto ff : f) {
-																		//if (ff - ee >= *it6 - *it5 - n && ff - ee <= *it6 - *it5 + n) {
-																			//std::cout << "\nNADENO PREKLAPANJE!!	"<< match;
-																			return(aa - *it1);
-																			
-																		//}
-																	//}
-																}
-															}
-														}
-													}
-												}
-											}
 										}
 									}
+
 								}
 							}
 						}
@@ -178,24 +157,24 @@ namespace mapping {
 		if (k1.ordering_number_for_string == k2.ordering_number_for_string){
 			return true;
 			}
-		/*for (auto &a:bases) {
-			k1_count[a] = 0;
-			k2_count[a] = 0;
-		}*/
+		for (auto a : k1_count) {
+			
+			k1_count.at(a.first) = 0;
+			//k2_count.at(a.first) = 0;
+		}
 
 		//int j = 0;
 		int diff = 0;
 		for (std::string::size_type i = 0; i < k1.string.size(); ++i) {
-			if (k1.string[i] != k2.string[i]){
-				diff++;
-				}
+			k1_count.at(k1.string[i])++;
+			k1_count.at(k2.string[i])--;
 		}
-		return diff < mutation_number*2;
+		//return diff < mutation_number * 2;
 		//finished counting;
-		/*if (abs(k1_count['A'] - k2_count['A']) + abs(k1_count['T'] - k2_count['T']) + abs(k1_count['C'] - k2_count['C']) + abs(k1_count['G'] - k2_count['G']) > mutation_number) {
+		if (abs(k1_count['A'])+ abs(k1_count['T']) + abs(k1_count['C']) + abs(k1_count['G'])  > mutation_number*2) {
 			return false;
 		}
-		return true;*/
+		return true;
 	}
 	
 }
